4da040ed72d58daab1de486ca2279b25c90bbfcf
jane-le <janele913@gmail.com>
2021-05-09 04:32:42 -0400

    configure form

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask, request, Response
+from flask import Flask, request, Response, make_response
 from slackeventsapi import SlackEventAdapter
 from datetime import datetime, timedelta
 import time
+import json
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -19,13 +20,173 @@ SCHEDULED_MESSAGES = [
     {'text': 'Hi', 'post_at': (datetime.now() + timedelta(seconds=30)).timestamp(), 'channel': 'C0216QC0JQ3'},
 ]
 
+DIALOG_FORM = {
+    "title": "Set Lumi's Icebreakers!",
+    "submit_label": "Submit",
+    "callback_id": "icebreaker_form",
+    "elements": [
+        {
+            "label": "How often? Every: ",
+            "type": "select",
+            "name": "frequency",
+            "placeholder": "Day",
+            "options": [
+                {
+                    "label": "Weekday",
+                    "value": "weekday"
+                },
+                {
+                    "label": "Day",
+                    "value": "day"
+                },
+            ]
+        },
+    ]
+}
+SCHEDULE_FORM_TITLE = "Let's configure your icebreakers! :smile_cat:"
+SCHEDULE_FORM = [
+    {
+        "text": "Step :one: : Choose your frequency!",
+        "fallback": "You are unable to choose a game",
+        "callback_id": "freq_selection",
+        "color": "#2ba197",
+        "attachment_type": "default",
+        "actions": [
+            {
+                "name": "game",
+                "text": "Weekdays",
+                "type": "button",
+                "value": "weekday"
+            },
+            {
+                "name": "game",
+                "text": "Everyday",
+                "type": "button",
+                "value": "day"
+            }
+        ]
+    },
+    {
+        "text": "Step :two::  Choose what time to display your icebreakers!",
+        "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
+        "color": "#2ba197",
+        "attachment_type": "default",
+        "callback_id": "time_selection",
+        "actions": [
+            {
+                "name": "games_list",
+                "text": "Pick a game...",
+                "type": "select",
+                "options": [
+                    {
+                        "text": "12:00 AM",
+                        "value": 0
+                    },
+                    {
+                        "text": "1:00 AM",
+                        "value": 1
+                    },
+                    {
+                        "text": "2:00 AM",
+                        "value": 2
+                    },
+                    {
+                        "text": "3:00 AM",
+                        "value": 3
+                    },
+                    {
+                        "text": "4:00 AM",
+                        "value": 4
+                    },
+                    {
+                        "text": "5:00 AM",
+                        "value": 5
+                    },
+                    {
+                        "text": "6:00 AM",
+                        "value": 6
+                    },
+                    {
+                        "text": "7:00 AM",
+                        "value": 7
+                    },
+                    {
+                        "text": "8:00 AM",
+                        "value": 8
+                    },
+                    {
+                        "text": "9:00 AM",
+                        "value": 9
+                    },
+                    {
+                        "text": "10:00 AM",
+                        "value": 10
+                    },
+                    {
+                        "text": "11:00 AM",
+                        "value": 11
+                    },
+                    {
+                        "text": "12:00 PM",
+                        "value": 12
+                    },
+                    {
+                        "text": "1:00 PM",
+                        "value": 13
+                    },
+                    {
+                        "text": "2:00 PM",
+                        "value": 14
+                    },
+                    {
+                        "text": "3:00 PM",
+                        "value": 15
+                    },
+                    {
+                        "text": "4:00 PM",
+                        "value": 16
+                    },
+                    {
+                        "text": "5:00 PM",
+                        "value": 17
+                    },
+                    {
+                        "text": "6:00 PM",
+                        "value": 18
+                    },
+                    {
+                        "text": "7:00 PM",
+                        "value": 19
+                    },
+                    {
+                        "text": "8:00 PM",
+                        "value": 20
+                    },
+                    {
+                        "text": "9:00 PM",
+                        "value": 21
+                    },
+                    {
+                        "text": "10:00 PM",
+                        "value": 22
+                    },
+                    {
+                        "text": "11:00 PM",
+                        "value": 23
+                    }
+                ]
+            }
+        ]
+    }
+]
+
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-    
+
     if BOT_ID != user_id:
         client.chat_postMessage(channel=channel_id, text=text)
 
@@ -43,6 +204,24 @@ def icebreaker():
     client.chat_postMessage(channel=data.get('channel_id'), text="What is your favourite colour?")
     return Response(), 200
 
+@app.route('/set-icebreakers', methods=['POST'])
+def schedule_icebreakers():
+    data = request.form
+    print(data)
+    # Show the ordering dialog to the user
+    # """open_dialog = client.dialog_open(dialog=DIALOG_FORM,trigger_id=data.get("trigger_id"),) """
+    client.chat_postMessage(channel=data.get('channel_id'), user=data.get('user_id'), text=SCHEDULE_FORM_TITLE,attachments=SCHEDULE_FORM)
+    return Response(), 200
+
+@app.route('/slack/interactive-endpoint', methods=['POST'])
+def set_icebreakers():
+    data = json.loads(request.form.get('payload'))
+    print(data['message_ts'])
+    if data['attachment_id'] == '2':
+        client.chat_update(channel=data['channel']['id'], ts=data['message_ts'], text="thanks!", attachments=[])
+   # client.chat_postEphemeral(channel=data.get('channel').get, user=data.get('user_id'), text=bot_response)
+    return Response(), 200
+
 def get_username(data):
     return client.users_info(user=data.get('user_id')).get("user").get("profile").get("first_name")
 
@@ -55,5 +234,5 @@ def schedule_icebreakers(icebreakers):
     return ids
 
 if __name__ == "__main__":
-    schedule_icebreakers(SCHEDULED_MESSAGES)
+    #schedule_icebreakers(SCHEDULED_MESSAGES)
     app.run(debug=True)
